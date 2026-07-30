60d03ba853038076f132c7520dedaa00ee332528
Sorathiya Nabhag <nabhagsorathiya81@gmail.com>
2026-07-30 12:06:44 +0530

    Update FacultyLayout.jsx
    removed unneccesary comment

--- src/Layouts/FacultyLayout.jsx
+++ src/Layouts/FacultyLayout.jsx
@@ -9,10 +9,8 @@ const FacultyLayout = () => {
       {/* Sidebar */}
       <FacultyNavbar />
 
-      {/* Main Content Area */}
       <div className="app-main flex-1 flex flex-col overflow-hidden">
-        {/* Page Content */}
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+]        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
           <Outlet />
         </main>
 
